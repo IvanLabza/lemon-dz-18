914bc356ef9bbc5cf66697999f76a1fc6468c8b2
IvanLabza <vanalabza77@gmail.com>
2023-12-06 20:17:14 +0200

    main

--- lemon-dz-18/README.md
+++ lemon-dz-18/README.md
@@ -1,4 +1,4 @@
-# AngularStart
+# LemonDz18
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.5.
 
--- lemon-dz-18/src/app/app.component.spec.ts
+++ lemon-dz-18/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-start' title`, () => {
+  it(`should have the 'lemon-dz-18' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-start');
+    expect(app.title).toEqual('lemon-dz-18');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-start');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lemon-dz-18');
   });
 });
